d48211af035d73a3f9b56de4142ff8fcc6be00ff
AdilBl <87983718+AdilBl@users.noreply.github.com>
2022-09-15 14:37:20 +0200

    Rectification

--- Server/server/server.class.cpp
+++ Server/server/server.class.cpp
@@ -192,26 +192,6 @@ std::string Server::comp[4] =
         "QUIT"
 };
 
-void Harl::complain(std::string level)
-{
-    bool    check;
-
-    check = false;
-    void	(Server::*fonction[])( void ) = {
-            //
-	};
-    for(int i = 0; i < 4; i++)
-    {
-        void (Harl::*bugtoselect)( void ) = bugreportory[i];
-        if (commande == this->comp[i])
-        {
-            (this->*bugtoselect)();
-            check = true;
-        }
-    }
-    if (!check)
-        std::cout << "421 [NICKNAME] " << commande << " :Unknown command" << std::endl;
-}
 void    Server::switchcommande(std::string message, Client *User)
 {
     std::string commande;
